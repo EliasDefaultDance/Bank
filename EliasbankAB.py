da4b48cbf4889d7ece8d9a6fe7f2a9f349e0a9a3
Elias Dahlgren <elias.dahlgren@learnet.se>
2019-10-09 13:38:28 +0200

    Lade till inloggningsuppgifter i en .txt fil

--- EliasbankAB.py
+++ EliasbankAB.py
@@ -7,43 +7,44 @@ if userBankId != bankId: #Om användaren matchat det korrekta inloggninsnamnet s
     print("Fel BankId!")
     time.sleep(3)
     exit() #Om använderen inte matchat det korrekta inloggninsnamnet, avsluta
+else:
 
-menu = 0
-file = open("Saldo.txt", "r+")
-saldo = float(file.readline()) #Float - Data typ för decimal tal
-file.close()
+    menu = 0
+    file = open("Saldo.txt", "r+")
+    saldo = float(file.readline()) #Float - Data typ för decimal tal
+    file.close()
 
-while menu == 0:
+    while menu == 0:
 
-    menu = int(input("Menu: 1.deposit 2.withdraw 3.saldo 4.logout")) # Alternativ beroende på ärende
-    if menu == 1:
-        file = open("saldo.txt", "w") # Öppnar saldofilen med write
-        deposit = float(input("Deposit, how much? ")) # Användare får skriva float för hur mycket hen ska lägga in
-        saldo = saldo + deposit # Uträkning
-        file.write(str(saldo)) # Skriver in Uträkningsresultatet i filen
-        file.close() # Stänger fil
-        print(saldo) # Printar saldo
-        menu = 0 # återgå till loopens början
+        menu = int(input("Menu: 1.deposit 2.withdraw 3.saldo 4.logout")) # Alternativ beroende på ärende
+        if menu == 1:
+            file = open("saldo.txt", "w") # Öppnar saldofilen med write
+            deposit = float(input("Deposit, how much? ")) # Användare får skriva float för hur mycket hen ska lägga in
+            saldo = saldo + deposit # Uträkning
+            file.write(str(saldo)) # Skriver in Uträkningsresultatet i filen
+            file.close() # Stänger fil
+            print(saldo) # Printar saldo
+            menu = 0 # återgå till loopens början
 
-    elif menu == 2:
-        file = open("saldo.txt", "w")
-        withdraw = float(input("Withdraw, how much? "))
-        saldo = saldo - withdraw #uträkning
-        if saldo <= 0: # Ifall 'saldo' är mindre än noll (-) printa observeringsmeddelande
-            print("OBSERVE! Negative balance!")
-        print(saldo)
-        file.write(str(saldo))
-        file.close()
-        menu = 0
+        elif menu == 2:
+            file = open("saldo.txt", "w")
+            withdraw = float(input("Withdraw, how much? "))
+            saldo = saldo - withdraw #uträkning
+            if saldo <= 0: # Ifall 'saldo' är mindre än noll (-) printa observeringsmeddelande
+                print("OBSERVE! Negative balance!")
+            print(saldo)
+            file.write(str(saldo))
+            file.close()
+            menu = 0
 
-    elif menu == 3:
-        print(saldo)
-        time.sleep(1) # Vänta 1 sek tills den återgår till menu 0
-        menu = 0
-    elif menu == 4:
-        print ("Logging out...")
-        time.sleep(3)   # Väntar 5 sekunder innan utloggning.
-        exit()
+        elif menu == 3:
+            print(saldo)
+            time.sleep(1) # Vänta 1 sek tills den återgår till menu 0
+            menu = 0
+        elif menu == 4:
+            print ("Logging out...")
+            time.sleep(3)   # Väntar 5 sekunder innan utloggning.
+            exit()
 
-    else:
-         menu = 0 # ifall skrivfel, återgå till loopens början
+        else:
+            menu = 0 # ifall skrivfel, återgå till loopens början
